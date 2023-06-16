dee748017557ae3a4f5085ca90948222d9b99202
MUKUNZI Jean De Dieu <jeandedieumukunzi7@gmail.com>
2023-06-16 12:39:39 -0700

    coding

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,9 +4,13 @@ int main() {
     // Print all possible different combinations of two digits
     for (int i = 0; i <= 8; i++) {
         for (int j = i + 1; j <= 9; j++) {
-            // Convert the digits to characters
-            char digit1 = i + '0';
-            char digit2 = j + '0';
+            // Convert the digits to ASCII values
+            int digit1 = i + '0';
+            int digit2 = j + '0';
+            
+            // Check if the combination is equivalent to 01 or 10
+            if ((digit1 == '0' && digit2 == '1') || (digit1 == '1' && digit2 == '0'))
+                continue;
             
             // Print the combination in ascending order
             putchar(digit1);
