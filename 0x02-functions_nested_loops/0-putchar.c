9ece6407d9de3a3b4c55bb62927f37acb6ccf172
MUKUNZI Jean De Dieu <jeandedieumukunzi7@gmail.com>
2023-06-20 16:50:46 -0700

    work

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,18 +6,18 @@
  * Description: This function prints the string "_putchar"
  *              using the _putchar function.
  *
- * Return: 0 on successful execution
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
