50fb625941c7c5282a9c4ddc6c47167cdd07159b
MUKUNZI Jean De Dieu <jeandedieumukunzi7@gmail.com>
2023-06-20 16:47:24 -0700

    work

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,23 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
 
-
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the string "_putchar"
+ *              using the _putchar function.
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
+}
