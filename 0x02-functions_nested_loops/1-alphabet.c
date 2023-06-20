31d1e7055a21f5a43421b95efb07b966c4bae47f
MUKUNZI Jean De Dieu <jeandedieumukunzi7@gmail.com>
2023-06-20 16:59:26 -0700

    work

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Prints all alphabets in lowercase
+ *
+ * Description: This function prints all the lowercase alphabets from 'a' to 'z'
+ *              using the _putchar function.
+ */
+void print_alphabet(void)
+{
+	char letter;
 
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
-
-	void print_alphabet(void)
-	{
-		char letter;
-
-
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-
-
-		_putchar('\n');
-	}
+	_putchar('\n');
+}
