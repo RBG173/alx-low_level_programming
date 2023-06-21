38920a52b6ea0f39df632579ccdb510c41219135
MUKUNZI Jean De Dieu <jeandedieumukunzi7@gmail.com>
2023-06-21 00:58:23 -0700

    work

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,46 +1,34 @@
 #include <stdio.h>
-#include "main.h"
+	#include "main.h"
 
-/**
- * print_to_98 - Prints all natural numbers from n to 98, followed by a new line.
- * @n: The number to start printing from.
- *
- * Description: This function prints all natural numbers from the given number 'n'
- *              up to 98, inclusively. If 'n' is less than or equal to 98, it will
- *              print all numbers from 'n' to 98 in ascending order. If 'n' is greater
- *              than 98, it will print all numbers from 'n' to 98 in descending order.
- */
-void print_to_98(int n)
-{
-	int i;
 
-	if (n <= 98)
+	/**
+	 * print_to_98 - prints all natural numbers from n to 98,
+	 * followed by a new line
+	 * @n: print from this number
+	 */
+	void print_to_98(int n)
 	{
-		for (i = n; i <= 98; i++)
+		int i, j;
+
+
+		if (n <= 98)
 		{
-			if (i != 98)
-				printf("%d, ", i);
-			else
-				printf("%d\n", i);
+			for (i = n; i <= 98; i++)
+			{
+				if (i != 98)
+					printf("%d, ", i);
+				else if (i == 98)
+					printf("%d\n", i);
+			}
+		} else if (n >= 98)
+		{
+			for (j = n; j >= 98; j--)
+			{
+				if (j != 98)
+					printf("%d, ", j);
+				else if (j == 98)
+					printf("%d\n", j);
+			}
 		}
 	}
-	else
-	{
-		for (i = n; i >= 98; i--)
-		{
-			if (i != 98)
-				printf("%d, ", i);
-			else
-				printf("%d\n", i);
-		}
-	}
-}
-
-int main(void)
-{
-	print_to_98(111);
-	print_to_98(81);
-	print_to_98(-10);
-
-	return 0;
-}
