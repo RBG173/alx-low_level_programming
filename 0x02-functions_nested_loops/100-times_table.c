4ad4aa3972e37d35914e2fbb5399204861186185
MUKUNZI Jean De Dieu <jeandedieumukunzi7@gmail.com>
2023-06-21 02:21:50 -0700

    work

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,53 +3,42 @@
 /**
  * print_times_table - Prints the n times table, starting with 0.
  * @n: Number of the times table.
- *
- * Description:
- * This function prints the times table for a given number 'n',
- * starting from 0 up to 'n' times.
  */
 void print_times_table(int n)
 {
-	int i, j, k;
-
 	if (n >= 0 && n <= 15)
 	{
+		int i, j;
+
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar('0');
+
+			for (j = 1; j <= n; j++)
 			{
-				k = j * i;
-				if (j == 0)
+				_putchar(',');
+				_putchar(' ');
+
+				if ((i * j) < 10)
 				{
-					_putchar(k + '0');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((i * j) + '0');
 				}
-				else if (k < 10 && j != 0)
+				else if ((i * j) < 100)
 				{
-					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
+					_putchar((i * j) / 10 + '0');
+					_putchar((i * j) % 10 + '0');
 				}
-				else if (k >= 10 && k < 100)
+				else
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+					_putchar((i * j) / 100 + '0');
+					_putchar(((i * j) / 10) % 10 + '0');
+					_putchar((i * j) % 10 + '0');
 				}
 			}
 			_putchar('\n');
 		}
 	}
 }
-
