416ed211e28fddbcfa45c66aed92d424af3f39d8
MUKUNZI Jean De Dieu <jeandedieumukunzi7@gmail.com>
2023-06-20 18:38:25 -0700

    work

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,6 +4,11 @@
 /**
  * print_to_98 - Prints all natural numbers from n to 98, followed by a new line.
  * @n: The number to start printing from.
+ *
+ * Description: This function prints all natural numbers from the given number 'n'
+ *              up to 98, inclusively. If 'n' is less than or equal to 98, it will
+ *              print all numbers from 'n' to 98 in ascending order. If 'n' is greater
+ *              than 98, it will print all numbers from 'n' to 98 in descending order.
  */
 void print_to_98(int n)
 {
