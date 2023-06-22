cfc69a9b95dff860c30b20469a3a8f51484ffd14
MUKUNZI Jean De Dieu <jeandedieumukunzi7@gmail.com>
2023-06-22 08:36:28 -0700

    memory

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - Tests the function for determining if a number is positive or negative.
- *
- * Return: Always 0.
+ * main - Test function for positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
-	int i;
+ int i;
 
-	i = 0;
-	positive_or_negative(i);
+ i = 0;
+ positive_or_negative(i);
 
-	return (0);
+return (0);
 }
